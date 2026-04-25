56f34a62aab4aac9a15442324be78b49cad4bf96
Darshan <umdarshan949@gmail.com>
2026-04-25 11:29:30 +0530

    cors added

--- server/src/server.js
+++ server/src/server.js
@@ -2,6 +2,17 @@ import "dotenv/config";
 import app from "./app.js";
 import connectDB from "./config/db.config.js";
 
+import cors from "cors";
+
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://ai-code-tanslator.vercel.app"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
+
 const PORT = process.env.PORT || 5000;
 
 const startServer = async () => {
